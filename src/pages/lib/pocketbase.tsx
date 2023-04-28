8a82262dc6b929d7ce6a8eab543013c2fa57d5bd
Levente Botos <66298417+LeventeBotos@users.noreply.github.com>
2023-04-28 16:42:05 +0200

    now rly

--- src/pages/lib/pocketbase.tsx
+++ src/pages/lib/pocketbase.tsx
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase('http://80.98.246.4:443'); 
+export const pb = new PocketBase('https://80.98.246.4:443'); 
 
 export const currentUser = writable(pb.authStore.model);
 
