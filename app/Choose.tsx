5cb4b080c6c8b8ad8d63e440f6672ef96153e13f
Levente Botos <botos.levente2007@gmail.com>
2023-10-16 13:35:27 +0200

    improved

--- app/Choose.tsx
+++ app/Choose.tsx
@@ -1,53 +1,55 @@
-"use client";
 import React, { useEffect, useState } from "react";
 import { db } from "./Firebase";
 import {
   addDoc,
   collection,
   deleteDoc,
-  getDoc,
   getDocs,
-  limit,
   query,
+  limit,
 } from "firebase/firestore";
 
 const Choose = () => {
   const [theme, setTheme] = useState("");
   const [count, setCount] = useState(0);
-  let id = "";
+  const [themes, setThemes] = useState<string[]>([]);
 
   useEffect(() => {
-    console.log(theme, count);
+    const fetchData = async () => {
+      try {
+        const q = collection(db, "themes");
+        const querySnapshot = await getDocs(q);
+        console.log(querySnapshot);
+        const themeData = querySnapshot.docs.map((doc) =>
+          String(doc.data().name)
+        ) as string[];
+        setThemes(themeData);
+        console.log(themeData);
+      } catch (error) {
+        console.error("Error fetching themes:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
     async function inner() {
       try {
+        if (!theme) return; // Don't proceed if theme is empty
+
         const querie = collection(db, theme);
         const q = await getDocs(querie);
 
-        // Calculate the count outside of the forEach loop
-        let newCount = 0;
-        q.forEach((doc) => {
-          newCount++;
-        });
+        const newCount = q.size;
 
         if (newCount === 0) {
-          // Generate a random UUID
-          id =
-            Math.random().toString(36).substring(2, 15) +
-            Math.random().toString(36).substring(2, 15);
+          const id = theme + Math.random().toString(36).substring(2, 15);
 
           // Add a document to the collection
-          // Check if 'theme' and 'id' have valid values
-          if (theme) {
-            await addDoc(collection(db, theme), { theme: theme, idd: id });
-            location.replace(`/chats/${id}`);
-          } else {
-            console.error("Invalid values for theme or id:", theme, id);
-          }
-
-          // Update the count state
-          setCount(newCount + 1);
+          await addDoc(collection(db, theme), { theme: theme, idd: id });
+          location.replace(`/chats/${id}`);
         } else {
-          // Query for a document and delete it
           const ref = query(collection(db, theme), limit(1));
           const querySnapshot = await getDocs(ref);
 
@@ -69,63 +71,25 @@ const Choose = () => {
       }
     }
     inner();
-  }, [theme, id]);
+  }, [theme]);
+
   return (
     <div className="flex p-5 gap-5 w-full flex-col h-screen justify-center items-center">
       <p className="text-xl md:text-3xl w-2/3 font-bold gradient">
         What do you want to talk about?
       </p>
       <div className="grid grid-cols-1 text-2xl w-full md:grid-cols-2 xl:grid-cols-4 content-evenly place-content-evenly gap-10 self-center h-full justify-center">
-        <div
-          onClick={() => {
-            setTheme("whatever");
-          }}
-          className="hover:cursor-pointer hover:bg-[#323232] ease-in-out duration-300 rounded-lg text-center text-2xl w-full h-32 bg-[#222222] flex flex-col justify-evenly"
-        >
-          Whtevr
-        </div>
-        <div
-          onClick={() => {
-            setTheme("day");
-          }}
-          className="hover:cursor-pointer hover:bg-[#323232] ease-in-out duration-300 rounded-lg text-center w-full h-32 bg-[#222222] flex flex-col justify-evenly"
-        >
-          Your day
-        </div>
-        <div
-          onClick={() => {
-            setTheme("testers");
-          }}
-          className="hover:cursor-pointer hover:bg-[#323232] ease-in-out duration-300 rounded-lg text-center w-full h-32 bg-[#222222] flex flex-col justify-evenly"
-        >
-          Test
-        </div>
-        <div
-          onClick={() => {
-            setTheme("hiers");
-          }}
-          className=" rounded-lg text-center text-6xl font-bold w-full h-32 bg-[#222222] flex flex-col justify-evenly"
-        >
-          hi
-        </div>
-        <div className=" rounded-lg text-center text-6xl font-bold w-full h-32 bg-[#222222] flex flex-col justify-evenly">
-          hi
-        </div>
-        <div className=" rounded-lg text-center text-6xl font-bold w-full h-32 bg-[#222222] flex flex-col justify-evenly">
-          hi
-        </div>
-        <div className=" rounded-lg text-center text-6xl font-bold w-full h-32 bg-[#222222] flex flex-col justify-evenly">
-          hi
-        </div>
-        <div className=" rounded-lg text-center text-6xl font-bold w-full h-32 bg-[#222222] flex flex-col justify-evenly">
-          hi
-        </div>
-        <div className=" rounded-lg text-center text-6xl font-bold w-full h-32 bg-[#222222] flex flex-col justify-evenly">
-          hi
-        </div>
-        <div className=" rounded-lg text-center text-6xl font-bold w-full h-32 bg-[#222222] flex flex-col justify-evenly">
-          hi
-        </div>
+        {themes.map((name, index) => (
+          <div
+            onClick={() => {
+              setTheme(name);
+            }}
+            key={index}
+            className="hover:cursor-pointer hover:bg-[#323232] ease-in-out duration-300 rounded-lg text-center text-2xl w-full h-32 bg-[#222222] flex flex-col justify-evenly"
+          >
+            {name}
+          </div>
+        ))}
       </div>
     </div>
   );
